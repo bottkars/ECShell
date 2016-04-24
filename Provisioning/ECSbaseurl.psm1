54188cc61eb9035aef38c0ecc91c644f0c6f235f
Karsten Bott <Karsten.Bott@bottnet.de>
2016-04-24 06:34:50 +0200

    added baseurl Post functions

--- Provisioning/ECSbaseurl.psm1
+++ Provisioning/ECSbaseurl.psm1
@@ -67,3 +67,51 @@ function Get-ECSbaseurldetail
 
     }
 }
+
+function New-ECSbaseurl
+{
+    [CmdletBinding(DefaultParameterSetName = '0')]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [alias('name')][string]$urlname,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [alias('link')][string]$url,
+        [switch]$is_namespace_in_host
+    )
+    Begin
+    {
+    $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    $class = "object/baseurl.json"
+    #$Excludeproperties = ('link','name','id')
+    $Expandproperty = "base_url"
+    $ContentType = "application/json"
+    $Method = "Post"
+    }
+    Process
+    {
+    $Uri = "$ECSbaseurl/$class"
+        $JSonBody = [ordered]@{ 
+	is_namespace_in_host = $is_namespace_in_host.IsPresent
+	name = $urlname 
+	base_url = $url } | ConvertTo-Json 
+
+    
+    try
+        {
+        Write-Verbose $Uri
+        (Invoke-RestMethod -Uri $Uri -Headers $ECSAuthHeaders -Method $Method -Body $JSonBody -ContentType $ContentType ) #| Select-Object -ExpandProperty $Expandproperty
+        }
+    catch
+        {
+        Get-ECSWebException -ExceptionMessage $_
+        break
+        }
+    }
+    End
+    {
+
+    }
+}
+
+
