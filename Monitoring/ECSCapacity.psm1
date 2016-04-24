872443237cb7c769dbb32c3d8c3fdbb741df62c5
Karsten Bott <Karsten.Bott@bottnet.de>
2016-04-24 18:58:43 +0200

    added capacity from Momnitoring

---
+++ Monitoring/ECSCapacity.psm1
@@ -0,0 +1,37 @@
+﻿function Get-ECScapacity
+{
+    [CmdletBinding(DefaultParameterSetName = '1')]
+    Param
+    (
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("vARRAYid")]$StoragePoolID
+    )
+    Begin
+    {
+    $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    $class = "object/$Myself"
+    $Expandproperty = "alert"
+    $Excludeproperty = "id"
+    $ContentType = "application/json"
+    $Method = "Get"
+    }
+    Process
+    {
+    $Uri = "$ECSbaseurl/$class/$StoragePoolID.json"
+    try
+        {
+        Write-Verbose $Uri
+        Invoke-RestMethod -Uri $Uri -Headers $ECSAuthHeaders -Method $Method -ContentType $ContentType  # | Select-Object  -ExpandProperty $Expandproperty 
+        }
+    catch
+        {
+        Get-ECSWebException -ExceptionMessage $_
+        #$_.Exception.Message
+        break
+        }
+    }
+    End
+    {
+
+    }
+}
