7a3f05a75f657c7401901ddc2fefc50edf72ba84
Karsten Bott <Karsten.Bott@bottnet.de>
2016-04-24 07:16:45 +0200

    added commoditydaastore

---
+++ Provisioning/ECSDatastore.psm1
@@ -0,0 +1,76 @@
+﻿function Get-ECSDataStores
+{
+    [CmdletBinding(DefaultParameterSetName = '1')]
+    Param
+    (
+    )
+    Begin
+    {
+    $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    $class = "vdc/data-stores"
+    $Excludeproperty = "link"
+    $Expandproperty = "data_store"
+    $ContentType = "application/json"
+    $Uri = "$ECSbaseurl/$class.json"
+    $Method = "Get"
+    }
+    Process
+    {
+
+    try
+        {
+        Write-Verbose $Uri
+        Invoke-RestMethod -Uri $Uri -Headers $ECSAuthHeaders -Method $Method -ContentType $ContentType | Select-Object  -ExpandProperty $Expandproperty
+        }
+    catch
+        {
+        Get-ECSWebException -ExceptionMessage $_
+        #$_.Exception.Message
+        break
+        }
+    }
+    End
+    {
+
+    }
+}
+
+#GET /vdc/data-stores/commodity/{id}
+function Get-ECSCommodityDataStore
+{
+    [CmdletBinding(DefaultParameterSetName = '1')]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [alias('ID')]$CommodityID
+    )
+    Begin
+    {
+    $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    $class = "vdc/data-stores/commodity"
+    $Excludeproperty = "link"
+    $Expandproperty = "commodity_data_store"
+    $ContentType = "application/json"
+    $Method = "Get"
+    }
+    Process
+    {
+    $Uri = "$ECSbaseurl/$class/$CommodityID"
+
+    try
+        {
+        Write-Verbose $Uri
+        Invoke-RestMethod -Uri $Uri -Headers $ECSAuthHeaders -Method $Method -ContentType $ContentType  | Select-Object  -ExpandProperty $Expandproperty
+        }
+    catch
+        {
+        Get-ECSWebException -ExceptionMessage $_
+        #$_.Exception.Message
+        break
+        }
+    }
+    End
+    {
+
+    }
+}
