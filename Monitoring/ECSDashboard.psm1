47026168361c47f5d4edb6914bbd488339ace92a
Karsten Bott <Karsten.Bott@emc.com>
2016-04-25 13:56:38 +0200

    added disks from dashboard

--- Monitoring/ECSDashboard.psm1
+++ Monitoring/ECSDashboard.psm1
@@ -36,7 +36,7 @@ function Get-ECSLocalzoneDashboard
 
     }
 }
-function Get-ECSNodeDashboard
+function Get-ECSNodeDiskDashboard
 {
     [CmdletBinding(DefaultParameterSetName = '1')]
     Param
@@ -74,4 +74,41 @@ function Get-ECSNodeDashboard
     }
 }
 
+#GET /dashboard/nodes/{id}/disks
+function Get-ECSNodeDashboard
+{
+    [CmdletBinding(DefaultParameterSetName = '1')]
+    Param
+    (
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("Node")]$Nodeid
+    )
+    Begin
+    {
+    $Myself = $MyInvocation.MyCommand.Name.Substring(7)
+    $class = "dashboard/nodes"
+    $Expandproperty = "alert"
+    $Excludeproperty = "id"
+    $ContentType = "application/json"
+    $Method = "Get"
+    }
+    Process
+    {
+    $Uri = "$ECSbaseurl/$class/$Nodeid/disks?dataType=current"
+    try
+        {
+        Write-Verbose $Uri
+        Invoke-RestMethod -Uri $Uri -Headers $ECSAuthHeaders -Method $Method -ContentType $ContentType  # | Select-Object  -ExpandProperty $Expandproperty 
+        }
+    catch
+        {
+        Get-ECSWebException -ExceptionMessage $_
+        #$_.Exception.Message
+        break
+        }
+    }
+    End
+    {
 
+    }
+}
